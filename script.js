33b3c18b390b472652e90635dcda9b970a4677f5
Emily Lu <2yearuniofpeople@gmail.com>
2022-11-28 00:17:19 +0700

    Updated

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-const lists = document.querySelectorAll('.list-group li');
+const lists = document.querySelectorAll(".list-group li");
+let dropdown = document.querySelectorAll(".dropdown-content p");
 
-for(let i = 0; i < lists.length; i++) {
-    lists[i].addEventListener('click', () => {
-        let dropdown = document.querySelectorAll('.dropdown-content p');
-        let img = document.querySelectorAll('.dropbtn img');
-        // add Class
-        lists[i].classList.toggle('active-list');
-        img[i].classList.toggle('expand');
-        dropdown[i].classList.toggle('show');
-    });
+for (let i = 0; i < lists.length; i++) {
+  lists[i].addEventListener("click", () => {
+    let img = document.querySelectorAll(".dropbtn img");
+    // add Class
+    lists[i].classList.toggle("active-list");
+    img[i].classList.toggle("expand");
+    dropdown[i].classList.toggle("show");
+  });
 }
+
