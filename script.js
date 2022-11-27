6ff7d5a9571242365ad0b526c105042b98692d8e
jaromeaviles <jaromeaviles@gmail.com>
2022-11-27 23:43:51 +0800

    Accordion opens one at a time implemented

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-const lists = document.querySelectorAll('.list-group li');
+    const lists = document.querySelectorAll('.list-group li');
+    let dropdown = document.querySelectorAll('.dropdown-content p');
+    let img = document.querySelectorAll('.dropbtn img');
 
 for(let i = 0; i < lists.length; i++) {
-    lists[i].addEventListener('click', () => {
-        let dropdown = document.querySelectorAll('.dropdown-content p');
-        let img = document.querySelectorAll('.dropbtn img');
-        // add Class
-        lists[i].classList.toggle('active-list');
-        img[i].classList.toggle('expand');
-        dropdown[i].classList.toggle('show');
-    });
+
+    lists[i].onclick = function() {
+        var setClasses = !this.classList.contains('active-list');
+        setClass(lists, 'active-list', 'remove');
+        setClass(dropdown, 'show', 'remove');
+        setClass(img, 'expand', 'remove');
+
+        if (setClasses) {
+            this.classList.toggle("active-list");
+            let icon = this.children[0].children[0];
+            icon.classList.toggle('expand');
+            let dropContent = this.children[1].children[0];
+            dropContent.classList.toggle("show");
+        }
+    }
+
+    function setClass(els, className, fnName) {
+        for (var i = 0; i < els.length; i++) {
+            els[i].classList[fnName](className);
+        }
+    }
 }
