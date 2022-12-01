cdbe80876f786040d5a5f8326832d045f96a09d2
jaromeaviles <jaromeaviles@gmail.com>
2022-12-01 19:49:16 +0800

    Added comments on scripts

--- script.js
+++ script.js
@@ -2,14 +2,20 @@
     let dropdown = document.querySelectorAll('.dropdown-content p');
     let img = document.querySelectorAll('.dropbtn img');
 
+// Loop through list to get each list item
 for(let i = 0; i < lists.length; i++) {
 
+    // Adds click event for specific list
     lists[i].onclick = function() {
+        // Checks if list contains class of active-list added ! means false
+        // Basicall setClasses variable will now contain list item that DOES NOT contain active-list class
         var setClasses = !this.classList.contains('active-list');
+        // Use the setClass function here that created below. The setClass function will accept 3 arguments.
         setClass(lists, 'active-list', 'remove');
         setClass(dropdown, 'show', 'remove');
         setClass(img, 'expand', 'remove');
 
+        // This will run if setClasses variable is true
         if (setClasses) {
             this.classList.toggle("active-list");
             let icon = this.children[0].children[0];
@@ -19,6 +25,7 @@ for(let i = 0; i < lists.length; i++) {
         }
     }
 
+    // This where the magic happens. 
     function setClass(els, className, fnName) {
         for (var i = 0; i < els.length; i++) {
             els[i].classList[fnName](className);
