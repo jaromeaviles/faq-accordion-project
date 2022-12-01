c1745839ea2644a488ea54fde647a6a91f81e5db
Emily Lu <arden.mie@gmail.com>
2022-12-01 23:42:04 +0700

    Completed FAQ ACCORDION project

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const lists = document.querySelectorAll(".list-group li");
 let dropdown = document.querySelectorAll(".dropdown-content p");
 let img = document.querySelectorAll(".dropbtn img");
+let questions = document.querySelectorAll(".question");
 
 for (let i = 0; i < lists.length; i++) {
   lists[i].onclick = function () {
@@ -8,10 +9,13 @@ for (let i = 0; i < lists.length; i++) {
     setClass(lists, "active-list", "remove");
     setClass(dropdown, "show", "remove");
     setClass(img, "expand", "remove");
+    setClass(questions, "bold", "remove");
 
     if (setClasses) {
       this.classList.toggle("active-list");
-      let icon = this.children[0].children[0];
+      let question = this.children[0].children[0];
+      question.classList.toggle("bold");
+      let icon = this.children[0].children[1];
       icon.classList.toggle("expand");
       let dropContent = this.children[1].children[0];
       dropContent.classList.toggle("show");
