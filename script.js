4b35ff0b650977f26b044fca47d8dc3d5b93ccd6
Emily Lu <arden.mie@gmail.com>
2022-12-04 18:15:26 +0700

    Finished the first project

--- script.js
+++ script.js
@@ -4,32 +4,31 @@ let img = document.querySelectorAll(".dropbtn img");
 let questions = document.querySelectorAll(".question");
 
 // Loop through list to get each list item
-for(let i = 0; i < lists.length; i++) {
+for (let i = 0; i < lists.length; i++) {
+  // Adds click event for specific list
+  lists[i].onclick = function () {
+    // Checks if list contains class of active-list added ! means false
+    // Basically setClasses variable will now contain list item that DOES NOT contain active-list class
+    var setClasses = !this.classList.contains("active-list");
+    // Use the setClass function here that created below. The setClass function will accept 3 arguments.
+    setClass(lists, "active-list", "remove");
+    setClass(dropdown, "show", "remove");
+    setClass(img, "expand", "remove");
 
-    // Adds click event for specific list
-    lists[i].onclick = function() {
-        // Checks if list contains class of active-list added ! means false
-        // Basically setClasses variable will now contain list item that DOES NOT contain active-list class
-        var setClasses = !this.classList.contains('active-list');
-        // Use the setClass function here that created below. The setClass function will accept 3 arguments.
-        setClass(lists, 'active-list', 'remove');
-        setClass(dropdown, 'show', 'remove');
-        setClass(img, 'expand', 'remove');
-
-        // This will run if setClasses variable is true
-        if (setClasses) {
-            this.classList.toggle("active-list");
-            let icon = this.children[0].children[1];
-            icon.classList.toggle('expand');
-            let dropContent = this.children[1].children[0];
-            dropContent.classList.toggle("show");
-        }
+    // This will run if setClasses variable is true
+    if (setClasses) {
+      this.classList.toggle("active-list");
+      let icon = this.children[0].children[1];
+      icon.classList.toggle("expand");
+      let dropContent = this.children[1].children[0];
+      dropContent.classList.toggle("show");
     }
+  };
+}
+
+// This where the magic happens.
+function setClass(els, className, fnName) {
+  for (var i = 0; i < els.length; i++) {
+    els[i].classList[fnName](className);
   }
-
-    // This where the magic happens. 
-    function setClass(els, className, fnName) {
-        for (var i = 0; i < els.length; i++) {
-            els[i].classList[fnName](className);
-        }
-    }
+}
